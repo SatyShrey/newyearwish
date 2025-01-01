7c1a28e57f5d7513dbc2cfe0f964d65ffb85025f
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-01-02 02:18:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const nam=urlParams.get('nam')
 if(nam){document.querySelector('.from').innerHTML=`Wishes from <b class='nam'>${nam}</b>`}
 const nam2=urlParams.get('nam2')
 document.querySelector('.nam2').innerHTML=nam2
-//const url="https://satyahappynewyearwish2025.netlify.app/"
-const url="http://127.0.0.1:5500/index.html"
+const url="https://satyahappynewyearwish2025.netlify.app/"
+//const url="http://127.0.0.1:5500/index.html"
 function wish(){
     if(document.getElementById('nam').value.trim()===''){
       alert('Enter your name')
