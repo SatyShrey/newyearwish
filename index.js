07ee38c07aa2c773cd0665711ef4b4c79b474d94
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-01-02 02:14:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,22 @@
 const mykeyValues=window.location.search
 const urlParams=new URLSearchParams(mykeyValues)
 const nam=urlParams.get('nam')
-document.querySelector('.nam').innerHTML=nam
-const url="https://satyahappynewyearwish2025.netlify.app/"
-function share(){
-    navigator.share({
-      title:"Wishes",
-      url:url+"?nam="+document.getElementById('nam').value
-    })
+if(nam){document.querySelector('.from').innerHTML=`Wishes from <b class='nam'>${nam}</b>`}
+const nam2=urlParams.get('nam2')
+document.querySelector('.nam2').innerHTML=nam2
+//const url="https://satyahappynewyearwish2025.netlify.app/"
+const url="http://127.0.0.1:5500/index.html"
+function wish(){
+    if(document.getElementById('nam').value.trim()===''){
+      alert('Enter your name')
+    }
+    else if(document.getElementById('nam2').value.trim()===''){
+      alert("Enter your friend's name")
+    }
+    else{
+      navigator.share({
+        title:"Wishes",
+        url:url+"?nam="+document.getElementById('nam').value+"&nam2="+document.getElementById('nam2').value
+      })
+    }
 }
