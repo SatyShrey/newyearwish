86f8aa9ed888476172a2f06e70300c6c0a90db91
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-01-02 14:01:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const nam=urlParams.get('nam')
 if(nam){document.querySelector('.from').innerHTML=`Wishes from <b class='nam'>${nam}</b>`}
 const nam2=urlParams.get('nam2')
 document.querySelector('.nam2').innerHTML=nam2
-const url="https://satyahappynewyearwish2025.netlify.app/"
-//const url="http://127.0.0.1:5500/index.html"
+//const url="https://satyahappynewyearwish2025.netlify.app/"
+const url="http://127.0.0.1:5500/index.html"
 function wish(){
     if(document.getElementById('nam').value.trim()===''){
-      alert('Enter your name')
+      alert2('Enter your name',document.getElementById('nam'))
     }
     else if(document.getElementById('nam2').value.trim()===''){
-      alert("Enter your friend's name")
+      alert2("Enter your friend's name",document.getElementById('nam2'))
     }
     else{
       navigator.share({
@@ -21,4 +21,17 @@ function wish(){
         url:url+"?nam="+document.getElementById('nam').value+"&nam2="+document.getElementById('nam2').value
       })
     }
+}
+
+function alert2(a,e){
+   e.placeholder=a;
+   e.style.boxShadow='0px 0px 30px red'
+   e.style.border='1px solid red'
+}
+function getNormal(e){
+   if(e.value.trim().length>0){
+   e.style.boxShadow='0px 0px 0px red'
+   e.style.border='0px solid red'
+    e.placeholder=''
+   }
 }
